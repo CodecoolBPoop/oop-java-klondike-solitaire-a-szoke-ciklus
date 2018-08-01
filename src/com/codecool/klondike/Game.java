e7e620011ff6412f8249d15f2031d3954f4b941f
havszab <havaldaszab@gmail.com>
2018-08-01 10:20:40 +0200

    Bugfix: Now faced-down cards aren't draggable (issue#7 solved)

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -66,6 +66,8 @@ public class Game extends Pane {
         Pile activePile = card.getContainingPile();
         if (activePile.getPileType() == Pile.PileType.STOCK) {
             return;
+        } else if(card.isFaceDown()) {
+            return;
         }
         double offsetX = e.getSceneX() - dragStartX;
         double offsetY = e.getSceneY() - dragStartY;
